4ab56f45eeb05a99f1a6c4bebab06f8fd644c031
= <=>
2020-11-08 18:22:18 +0300

    update_1

--- src/main/java/ru/ndg/practice/controller/GlobalExceptionHandler.java
+++ src/main/java/ru/ndg/practice/controller/GlobalExceptionHandler.java
@@ -4,13 +4,17 @@ import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
@@ -18,6 +22,11 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> exceptionHandler(Exception ex, WebRequest request) {
+        return new ResponseEntity<>(ControllerUtils.putViewInBody("error", ex.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         return new ResponseEntity<>(ControllerUtils.putViewInBody("error", ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
@@ -33,5 +42,18 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 .collect(Collectors.toList());
 
         return new ResponseEntity<>(ControllerUtils.putViewInBody("error", errors), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    protected ResponseEntity<Object> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex,
+                                                                      WebRequest request) {
+        String message = String.format("Параметр '%s' со значенением '%s' не может быть сконвертирован в тип '%s'",
+                ex.getName(), ex.getValue(), Objects.requireNonNull(ex.getRequiredType()).getSimpleName());
+        return new ResponseEntity<>(ControllerUtils.putViewInBody("error", message), HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return new ResponseEntity<>(ControllerUtils.putViewInBody("error", "Не найден обработчик запроса"), HttpStatus.BAD_REQUEST);
     }
 }
