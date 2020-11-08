5b91d43cb6064a0d798f51d1560cf081d069ed62
= <=>
2020-11-08 05:02:43 +0300

    update_1

--- src/main/java/ru/ndg/practice/controller/document/DocumentController.java
+++ src/main/java/ru/ndg/practice/controller/document/DocumentController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.ndg.practice.controller.DefaultController;
 import ru.ndg.practice.service.document.DocumentService;
 import ru.ndg.practice.service.user.UserService;
 import ru.ndg.practice.view.DocumentView;
@@ -15,7 +16,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping(value = {"/api/doc"})
-public class DocumentController {
+public class DocumentController implements DefaultController {
 
     private final DocumentService documentService;
 
@@ -27,32 +28,24 @@ public class DocumentController {
     @GetMapping(value = {"/list"})
     public ResponseEntity<Object> getAllDocuments() {
         List<DocumentView> allDocuments = documentService.getAllDocuments();
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("data", allDocuments);
-        return new ResponseEntity<>(body, HttpStatus.OK);
+        return new ResponseEntity<>(putViewInBody("data", allDocuments), HttpStatus.OK);
     }
 
     @GetMapping(value = {"/{id}"})
     public ResponseEntity<Object> getDocumentById(@PathVariable(name = "id") Integer id) {
         DocumentView document = documentService.getDocument(id);
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("data", document);
-        return new ResponseEntity<>(body, HttpStatus.OK);
+        return new ResponseEntity<>(putViewInBody("data", document), HttpStatus.OK);
     }
 
     @PostMapping(value = {"/update"})
     public ResponseEntity<Object> updateDocument(@RequestBody DocumentView documentView) {
         documentService.updateDocument(documentView);
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("result", "success");
-        return new ResponseEntity<>(body, HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(putViewInBody("data", "success"), HttpStatus.ACCEPTED);
     }
 
     @PostMapping(value = {"/save"})
     public ResponseEntity<Object> saveDocument(@RequestBody DocumentView documentView) {
         documentService.saveDocument(documentView);
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("result", "success");
-        return new ResponseEntity<>(body, HttpStatus.CREATED);
+        return new ResponseEntity<>(putViewInBody("data", "success"), HttpStatus.CREATED);
     }
 }
