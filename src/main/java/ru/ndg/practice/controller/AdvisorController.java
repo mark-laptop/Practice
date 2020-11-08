6e33944e5e05daf85dee8287fe83950a99122582
= <=>
2020-11-08 06:28:42 +0300

    update_1

--- src/main/java/ru/ndg/practice/controller/AdvisorController.java
+++
@@ -1,9 +0,0 @@
-package ru.ndg.practice.controller;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-@ControllerAdvice
-public class AdvisorController extends ResponseEntityExceptionHandler {
-
-}
---
+++ src/main/java/ru/ndg/practice/controller/ExceptionHandler.java
@@ -0,0 +1,8 @@
+package ru.ndg.practice.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ExceptionHandler {
+
+}
