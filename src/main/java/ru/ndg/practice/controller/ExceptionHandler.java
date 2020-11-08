6a6b80a221a5844bd3d3b42647eb17945ccee708
= <=>
2020-11-08 06:29:57 +0300

    update_1

--- src/main/java/ru/ndg/practice/controller/ExceptionHandler.java
+++ src/main/java/ru/ndg/practice/controller/ExceptionHandler.java
@@ -1,8 +1,9 @@
 package ru.ndg.practice.controller;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ExceptionHandler {
+public class ExceptionHandler extends ResponseEntityExceptionHandler {
 
 }
