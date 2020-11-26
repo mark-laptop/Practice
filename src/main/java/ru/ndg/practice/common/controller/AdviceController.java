84eeacf50d7500335d697b8e610340b2d02ab19f
= <=>
2020-11-26 17:06:36 +0300

    update_1

--- src/main/java/ru/ndg/practice/common/controller/AdviceController.java
+++ src/main/java/ru/ndg/practice/common/controller/AdviceController.java
@@ -2,6 +2,7 @@ package ru.ndg.practice.common.controller;
 
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,7 +15,7 @@ import java.util.Map;
 public class AdviceController implements ResponseBodyAdvice<Object> {
     @Override
     public boolean supports(MethodParameter returnType, Class converterType) {
-        return true;
+        return converterType == MappingJackson2HttpMessageConverter.class;
     }
 
     @Override
